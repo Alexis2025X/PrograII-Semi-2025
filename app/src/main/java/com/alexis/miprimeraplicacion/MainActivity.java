f8c7676a090d34302a340b34a60c9b775b23d568
KEVIN ALEXIS CASTILLO CASTILLO <USTR007724@ugb.edu.sv>
2025-01-27 12:49:53 -0600

    Creacion de la calculadora de suma

--- app/src/main/java/com/alexis/miprimeraplicacion/MainActivity.java
+++ app/src/main/java/com/alexis/miprimeraplicacion/MainActivity.java
@@ -1,6 +1,9 @@
 package com.alexis.miprimeraplicacion;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    //tamVal es un valor temporal
+    TextView temVal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+         //Buscamos el boton por su ID
+         btn = findViewById(R.id.btnCalcular);
+         btn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 temVal = findViewById(R.id.txtNum1);
+                 double num1 = Double.parseDouble(temVal.getText().toString());
+
+                 temVal = findViewById(R.id.txtNum2);
+                 double num2 = Double.parseDouble(temVal.getText().toString());
+
+                 double respuesta = num1 + num2;
+
+                 temVal = findViewById(R.id.lblRespuesta);
+                 temVal.setText("Respuesta: " + respuesta);
+
+             }
+         });
     }
 }
