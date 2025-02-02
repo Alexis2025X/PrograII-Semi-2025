7953a6f9ea10fbfc75cb19abf087ea160374b7ec
KEVIN ALEXIS CASTILLO CASTILLO <USTR007724@ugb.edu.sv>
2025-02-02 13:46:53 -0600

    Se agrego las opciones de suma, resta, multiplicacion, y division.

--- app/src/main/java/com/alexis/miprimeraplicacion/MainActivity.java
+++ app/src/main/java/com/alexis/miprimeraplicacion/MainActivity.java
@@ -1,8 +1,11 @@
 package com.alexis.miprimeraplicacion;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,28 +17,83 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     Button btn;
     //tamVal es un valor temporal
-    TextView temVal;
+    TextView tempVal;
+    RadioGroup rgo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
          //Buscamos el boton por su ID
          btn = findViewById(R.id.btnCalcular);
+
+         //El bloque de codigo se ejecutara al dar click en le boton
          btn.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
-                 temVal = findViewById(R.id.txtNum1);
-                 double num1 = Double.parseDouble(temVal.getText().toString());
+                    // temVal = findViewById(R.id.txtNum1);
+                    // double num1 = Double.parseDouble(temVal.getText().toString());
+                 //tempVal txt = findViewById()
 
-                 temVal = findViewById(R.id.txtNum2);
-                 double num2 = Double.parseDouble(temVal.getText().toString());
+                 tempVal = findViewById(R.id.txtNum1);
+                 double num1 = 0;
 
-                 double respuesta = num1 + num2;
 
-                 temVal = findViewById(R.id.lblRespuesta);
-                 temVal.setText("Respuesta: " + respuesta);
+                 if (tempVal.getText().toString().isEmpty()){
+                     num1 = 0;
+                 }else {
+                     num1 = Double.parseDouble(tempVal.getText().toString());
+                 }
+                 //Descomentarlo sino funciona
+                 tempVal = findViewById(R.id.txtNum2);
+                 double num2 = 0;
+                 double respuesta = 0;
 
-             }
+                 if (tempVal.getText().toString().isEmpty()){
+                     num2 = 0;
+                 }else {
+                     num2 = Double.parseDouble(tempVal.getText().toString());
+                 }
+
+                     tempVal = findViewById(R.id.lblRespuesta);
+
+                     rgo = findViewById(R.id.rgoOpciones);
+                     if (rgo.getCheckedRadioButtonId() == R.id.rgoSuma) {
+                         //tempVal = findViewById(R.id.txtNum2);
+                         //tempVal.setEnabled(true);
+                         respuesta = num1 + num2;
+
+                     } else if (rgo.getCheckedRadioButtonId() == R.id.rgoResta) {
+                         respuesta = num1 - num2;
+                     } else if (rgo.getCheckedRadioButtonId() == R.id.rgoMultiplicacion) {
+                         respuesta = num1 * num2;
+                     } else if (rgo.getCheckedRadioButtonId() == R.id.rgoDivision) {
+                         respuesta = num1 / num2;
+                     }
+                     //Parte 2
+                     else if (rgo.getCheckedRadioButtonId() == R.id.rgoExponencial) {
+                         respuesta = Math.pow(num1, num2);
+                     } else if (rgo.getCheckedRadioButtonId() == R.id.rgoPorcentaje) {
+                         respuesta = (num1 / num2) * 100;
+                     } else if (rgo.getCheckedRadioButtonId() == R.id.rgoRaiz) {
+                         //tempVal = findViewById(R.id.txtNum1);
+                        // tempVal.setHint("Indice" + R.id.txtNum1);
+                         respuesta = Math.pow(num2, 1.0 / num1);
+                       //  tempVal.setText("Respuesta: " + num1 +"√"+ num2 + "="+ respuesta);
+
+                     } else if (rgo.getCheckedRadioButtonId() == R.id.rgoFactorial) {
+                         //temVal = num2.SetEnabled(false);
+                         double fact = 1;
+                         for (double i = num1; i > 0; i--) {
+                             fact = fact * i;
+                         }
+                         respuesta = fact;
+                     }
+
+                     tempVal.setText("Respuesta: " + respuesta);
+                 }
+
          });
     }
+
 }
