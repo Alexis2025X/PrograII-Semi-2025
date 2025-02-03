9e8a9bc26e6cc94f1f273dc07258f485adf3823e
KEVIN ALEXIS CASTILLO CASTILLO <USTR007724@ugb.edu.sv>
2025-02-03 12:07:27 -0600

    Uso del spinner en la calculadora

--- app/src/main/java/com/alexis/miprimeraplicacion/MainActivity.java
+++ app/src/main/java/com/alexis/miprimeraplicacion/MainActivity.java
@@ -1,11 +1,11 @@
 package com.alexis.miprimeraplicacion;
 
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Switch;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -16,83 +16,74 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
-    //tamVal es un valor temporal
     TextView tempVal;
-    RadioGroup rgo;
+    Spinner spn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         //Buscamos el boton por su ID
-         btn = findViewById(R.id.btnCalcular);
+        btn = findViewById(R.id.btnCalcular);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tempVal = findViewById(R.id.txtNum1);
+                double num1 = 0;
+                if(tempVal.getText().toString().isEmpty()){
+                    num1 = 0;
+                }
+                else{
+                    num1 = Double.parseDouble(tempVal.getText().toString());
+                }
+                //Double.parseDouble(tempVal.getText().toString());
 
-         //El bloque de codigo se ejecutara al dar click en le boton
-         btn.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                    // temVal = findViewById(R.id.txtNum1);
-                    // double num1 = Double.parseDouble(temVal.getText().toString());
-                 //tempVal txt = findViewById()
+                tempVal = findViewById(R.id.txtNum2);
+                double num2 = 0;
+                if(tempVal.getText().toString().isEmpty()){
+                    num2 = 0;
+                }
+                else{
+                    num2 = Double.parseDouble(tempVal.getText().toString());
+                }
+                //Double.parseDouble(tempVal.getText().toString());
 
-                 tempVal = findViewById(R.id.txtNum1);
-                 double num1 = 0;
+                double respuesta = 0.0;
 
+                spn = findViewById(R.id.spnOpciones);
+                switch (spn.getSelectedItemPosition()){
+                    case 0:
+                        respuesta = num1 + num2;
+                        break;
+                    case 1:
+                        respuesta = num1 - num2;
+                        break;
+                    case 2:
+                        respuesta = num1 * num2;
+                        break;
+                    case 3:
+                        respuesta = num1 / num2;
+                        break;
+                    case 4:
+                        respuesta = Math.pow(num1, num2);
+                        break;
+                    case 5:
+                        respuesta = (num1 / num2) * 100;
+                        break;
+                    case 6:
+                        respuesta = Math.pow(num2, 1.0 / num1);
+                        break;
+                    case 7:
+                        double fact = 1;
+                        for (double i = num1; i > 0; i--) {
+                            fact = fact * i;
+                        }
+                        respuesta = fact;
+                        break;
 
-                 if (tempVal.getText().toString().isEmpty()){
-                     num1 = 0;
-                 }else {
-                     num1 = Double.parseDouble(tempVal.getText().toString());
-                 }
-                 //Descomentarlo sino funciona
-                 tempVal = findViewById(R.id.txtNum2);
-                 double num2 = 0;
-                 double respuesta = 0;
-
-                 if (tempVal.getText().toString().isEmpty()){
-                     num2 = 0;
-                 }else {
-                     num2 = Double.parseDouble(tempVal.getText().toString());
-                 }
-
-                     tempVal = findViewById(R.id.lblRespuesta);
-
-                     rgo = findViewById(R.id.rgoOpciones);
-                     if (rgo.getCheckedRadioButtonId() == R.id.rgoSuma) {
-                         //tempVal = findViewById(R.id.txtNum2);
-                         //tempVal.setEnabled(true);
-                         respuesta = num1 + num2;
-
-                     } else if (rgo.getCheckedRadioButtonId() == R.id.rgoResta) {
-                         respuesta = num1 - num2;
-                     } else if (rgo.getCheckedRadioButtonId() == R.id.rgoMultiplicacion) {
-                         respuesta = num1 * num2;
-                     } else if (rgo.getCheckedRadioButtonId() == R.id.rgoDivision) {
-                         respuesta = num1 / num2;
-                     }
-                     //Parte 2
-                     else if (rgo.getCheckedRadioButtonId() == R.id.rgoExponencial) {
-                         respuesta = Math.pow(num1, num2);
-                     } else if (rgo.getCheckedRadioButtonId() == R.id.rgoPorcentaje) {
-                         respuesta = (num1 / num2) * 100;
-                     } else if (rgo.getCheckedRadioButtonId() == R.id.rgoRaiz) {
-                         //tempVal = findViewById(R.id.txtNum1);
-                        // tempVal.setHint("Indice" + R.id.txtNum1);
-                         respuesta = Math.pow(num2, 1.0 / num1);
-                       //  tempVal.setText("Respuesta: " + num1 +"√"+ num2 + "="+ respuesta);
-
-                     } else if (rgo.getCheckedRadioButtonId() == R.id.rgoFactorial) {
-                         //temVal = num2.SetEnabled(false);
-                         double fact = 1;
-                         for (double i = num1; i > 0; i--) {
-                             fact = fact * i;
-                         }
-                         respuesta = fact;
-                     }
-                     tempVal.setText("Respuesta: " + respuesta);
-                 }
-
-         });
+                }
+                tempVal = findViewById(R.id.lblRespuesta);
+                tempVal.setText("Respuesta: "+ respuesta);
+            }
+        });
     }
-
 }
